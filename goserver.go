6fca987497cbc111c6900c35940fcffb62f71182
valderimedeiros <89540119+valderimedeiros@users.noreply.github.com>
2024-04-18 09:53:05 +0100

    Update goserver.go 09:52

--- goserver.go
+++ goserver.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"net/http",
+	"os"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -12,9 +13,12 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
+	port := os.Getenv("SERVER_PORT", "8080")
+	
 	fmt.Printf("START\n")
 	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
